33cd75d051f4c3f4de2696e9eb8c9b34f9c407a3
chadfennell <libsys@gmail.com>
2014-09-10 17:03:46 -0500

    default to using the app secret

--- config/initializers/blacklight_initializer.rb
+++ config/initializers/blacklight_initializer.rb
@@ -3,5 +3,5 @@
 # will use the application's secret key base instead.
 #
 
-# Blacklight.secret_key = 'a338e58ce77cddb632aa298b36bfe86464757379bda8eb83d7b1d4cbfa26dda21d33f8707fa032e330534f4b6b344bdb201ede8547ca5d749acbfe1f12ae63f8'
+Blacklight.secret_key = Rails.application.secrets.secret_key_base
 

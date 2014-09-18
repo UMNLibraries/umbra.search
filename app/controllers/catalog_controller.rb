8d92deacea12f98d4de7f04a72ec00847d4fc6f9
chadfennell <libsys@gmail.com>
2014-09-18 17:56:13 -0500

    remove collection until somebody asks for it

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -88,7 +88,6 @@ class CatalogController < ApplicationController
     config.add_show_field 'sourceResource_publisher_s', :label => 'Publisher'
     config.add_show_field 'language_facet', :label => 'Language'
     config.add_show_field 'sourceResource_extent_s', :label => 'Extent'
-    config.add_show_field 'sourceResource_collection_title_s', :label => 'Collection'
     config.add_show_field 'sourceResource_description_txt', :label => 'Description'
     config.add_show_field 'sourceResource_rights_s', :label => 'Rights'
     config.add_show_field 'sourceResource_format_s', :label => 'Format'

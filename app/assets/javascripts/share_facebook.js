3ae149cafd20600a5f3ae4785376f53f668ac335
chadfennell <libsys@gmail.com>
2014-10-23 19:30:39 -0500

    add basic support for social media links

---
+++ app/assets/javascripts/share_facebook.js
@@ -0,0 +1,15 @@
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '304349596442657',
+      xfbml      : true,
+      version    : 'v2.1'
+    });
+  };
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=304349596442657&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));

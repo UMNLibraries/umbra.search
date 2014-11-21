dfc1db64d9c72bf32b4ba65854a3b00e7d559e6a
chadfennell <libsys@gmail.com>
2014-11-21 11:39:07 -0600

    don't show thumbnail if no object is present in the solr doc

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -2,7 +2,13 @@ module ThumbnailHelper
 
   def cached_thumbnail_tag(document, image_options)
     #image_tag thumbnail_url(document), image_options
-    image_tag cached_thumbnail_url(url:thumbnail_url(document)), image_options
+    image_tag cached_thumbnail_url(url:thumbnail_url(document)), image_options if has_thumbnail?(document)
+  end
+
+  # Allows us to not show the thumbnail at all if the document thumbnail field
+  # is altogether missing
+  def has_thumbnail?(document)
+    document.has?(blacklight_config.view_config(document_index_view_type).thumbnail_field)
   end
 
 end

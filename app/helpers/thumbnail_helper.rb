4ffa502c688e8895a8e0699a217ed588f5d15d88
chadfennell <libsys@gmail.com>
2014-11-21 14:04:23 -0600

    whoops, namespace collision on the thumbnail check method.

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -2,12 +2,12 @@ module ThumbnailHelper
 
   def cached_thumbnail_tag(document, image_options)
     #image_tag thumbnail_url(document), image_options
-    image_tag cached_thumbnail_url(url:thumbnail_url(document)), image_options if has_thumbnail?(document)
+    image_tag cached_thumbnail_url(url:thumbnail_url(document)), image_options if has_thumbnail_field?(document)
   end
 
   # Allows us to not show the thumbnail at all if the document thumbnail field
   # is altogether missing
-  def has_thumbnail?(document)
+  def has_thumbnail_field?(document)
     document.has?(blacklight_config.view_config(document_index_view_type).thumbnail_field)
   end
 

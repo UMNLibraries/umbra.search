3c7e1297b5b6576b03a1bc279884a7458170f473
chadfennell <libsys@gmail.com>
2014-12-22 11:40:03 -0600

    looks like   config.action_mailer.default_url_options needs to be in application.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,8 +16,6 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  config.action_mailer.default_url_options = { host: ENV['AATH_MAILER_HOST'], port: ENV['AATH_MAILER_PORT'] }
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: "umbrasearch.org"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',

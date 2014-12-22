0532dfa14e57d9928aaa0bc4f916a458926d4302
chadfennell <libsys@gmail.com>
2014-12-22 11:03:13 -0600

    Configure devise to email to our umbrasearch gmail account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'umbrasearch.org',
+    user_name:            ENV['AATH_MAIL_USERNAME'],
+    password:             ENV['AATH_MAIL_PASSWORD'],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end

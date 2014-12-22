e231a50c74aff6c5f64dcb014723263e8c70b6bb
chadfennell <libsys@gmail.com>
2014-12-22 11:35:52 -0600

    the domain should be stable, no need to make this configurable on prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: ENV['AATH_MAILER_HOST'], port: ENV['AATH_MAILER_PORT'] }
+  config.action_mailer.default_url_options = { host: "umbrasearch.org"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',

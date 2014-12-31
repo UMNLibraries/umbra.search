e0256ca36de44b6b0f2f0ddd8cdab03bbb0aef47
chadfennell <libsys@gmail.com>
2014-12-31 15:42:18 -0600

    test editing of folder and folder item descriptions

--- spec/features/edit_folders_spec.rb
+++ spec/features/edit_folders_spec.rb
@@ -23,44 +23,73 @@ describe 'Editing a folder' do
     click_link "Edit Exhibit"
   end
 
-  it 'Should allow me to update the order of the exhibit items' do
-    expect(page).to have_field 'folder[items_attributes][0][position]', with: '1'
-    expect(page).to have_field 'folder[items_attributes][1][position]', with: '2'
+  context 'when logged in' do
+    it 'Should allow me to update the exhibit name and description' do
+      expect(page).to have_field 'folder[items_attributes][0][position]', with: '1'
+      expect(page).to have_field 'folder[items_attributes][1][position]', with: '2'
 
-    fill_in 'folder[items_attributes][1][position]', with: '1'
+      fill_in 'folder[name]', with: 'I just met you'
+      fill_in 'folder[description]', with: 'and this is crazy'
+      click_button "Update Exhibit"
 
-    click_button "Update Exhibit"
+      expect(page).to have_content "The exhibit was successfully updated"
 
-    expect(page).to have_content "The exhibit was successfully updated"
-
-    # check that the second item is now the first.
-    expect(page).to have_selector "#documents .folder-gallery-item .folder-item-title", text: 'Another title'
-  end
-
-  it "should allow me to remove exhibit items" do
-    within "#documents li:first-of-type" do
-      check 'Delete'
+      # check that the second item is now the first.
+      expect(page).to have_selector ".folder-view:first-of-type h1", text: 'I just met you'
+      expect(page).to have_selector ".description", text: 'and this is crazy'
     end
-    click_button "Update Exhibit"
 
-    expect(page).to have_content "The exhibit was successfully updated"
-    expect(page).not_to have_content "A title"
-    expect(page).to have_selector "#documents .folder-gallery-item", count: 1
-  end
+    it 'Should allow me to update the description of exhibit items' do
+      expect(page).to have_field 'folder[items_attributes][0][description]', with: ''
 
-  it "should allow me to move items from one exhibit to another" do
-    within "#documents li:first-of-type" do
-      select 'Destination Folder', from: 'folder[items_attributes][0][folder_id]'
+      fill_in 'folder[items_attributes][0][description]', with: 'Love Me Harder'
+      click_button "Update Exhibit"
+
+      expect(page).to have_content "The exhibit was successfully updated"
+
+      # check that the second item is now the first.
+      expect(page).to have_selector "#documents .folder-gallery-item .folder-item-description", text: 'Love Me Harder'
     end
-    click_button "Update Exhibit"
 
-    expect(page).to have_content "The exhibit was successfully updated"
-    expect(page).not_to have_content "A title"
-    expect(page).to have_selector "#documents .folder-gallery-item", count: 1
+    it 'Should allow me to update the order of the exhibit items' do
+      expect(page).to have_field 'folder[items_attributes][0][position]', with: '1'
+      expect(page).to have_field 'folder[items_attributes][1][position]', with: '2'
 
-    click_link 'Destination Folder'
-    within '#documents' do
-      expect(page).to have_content "A title"
+      fill_in 'folder[items_attributes][1][position]', with: '1'
+
+      click_button "Update Exhibit"
+
+      expect(page).to have_content "The exhibit was successfully updated"
+
+      # check that the second item is now the first.
+      expect(page).to have_selector "#documents .folder-gallery-item .folder-item-title", text: 'Another title'
+    end
+
+    it "should allow me to remove exhibit items" do
+      within "#documents li:first-of-type" do
+        check 'Delete'
+      end
+      click_button "Update Exhibit"
+
+      expect(page).to have_content "The exhibit was successfully updated"
+      expect(page).not_to have_content "A title"
+      expect(page).to have_selector "#documents .folder-gallery-item", count: 1
+    end
+
+    it "should allow me to move items from one exhibit to another" do
+      within "#documents li:first-of-type" do
+        select 'Destination Folder', from: 'folder[items_attributes][0][folder_id]'
+      end
+      click_button "Update Exhibit"
+
+      expect(page).to have_content "The exhibit was successfully updated"
+      expect(page).not_to have_content "A title"
+      expect(page).to have_selector "#documents .folder-gallery-item", count: 1
+
+      click_link 'Destination Folder'
+      within '#documents' do
+        expect(page).to have_content "A title"
+      end
     end
   end
 end

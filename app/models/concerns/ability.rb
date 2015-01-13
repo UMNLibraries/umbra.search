2294989ccd7d70f5da0b85a607bee9db76142281
chadfennell <libsys@gmail.com>
2015-01-13 14:20:08 -0600

    removing abilities. not using them for now. we'll need to include the existing BL folders abilities when we do so

--- app/models/concerns/ability.rb
+++
@@ -1,9 +0,0 @@
-class Ability
-  include CanCan::Ability
-
-  def initialize(user)
-    if user.has_role?("admin") || user.has_role?("librarian")
-      can :view_librarian_uis, :all
-    end
-  end
-end

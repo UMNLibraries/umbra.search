3823c08c288a7e240983516b0f13750edbd60147
chadfennell <libsys@gmail.com>
2015-02-24 14:16:39 -0600

    ensure thumbnails are used by social media as the default image

--- app/helpers/thumbnail_helper.rb
+++ app/helpers/thumbnail_helper.rb
@@ -2,7 +2,7 @@ module ThumbnailHelper
 
   def cached_thumbnail_tag(document, image_options)
     title = render_document_index_label document, label: document_show_link_field(document)
-    image_options = {class:"thumbnail", alt:title.strip, title:title.strip}.merge(image_options)
+    image_options = {class:"thumbnail", alt:title.strip, title:title.strip, property:"og:image"}.merge(image_options)
     if has_thumbnail_field?(document)
       image_tag cached_thumbnail_url(url:thumbnail_url(document)), image_options
     else

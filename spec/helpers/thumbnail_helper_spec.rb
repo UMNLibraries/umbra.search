7032be694ab85e1f651cffcb8d7c2de34726b74e
chadfennell <libsys@gmail.com>
2015-04-07 15:26:06 -0500

    update to the latest solr schema

--- spec/helpers/thumbnail_helper_spec.rb
+++ spec/helpers/thumbnail_helper_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe ThumbnailHelper, :type => :helper do
   let(:thumbnail_url) { "https://www.google.com/images/srpr/logo11w.png?foo=bar" }
-  let(:document) {SolrDocument.new( {object_s:thumbnail_url, title_display:"Awesome Title"})}
+  let(:document) {SolrDocument.new( {object_ssi:thumbnail_url, title_ssi:"Awesome Title"})}
   let(:blacklight_config) { CatalogController.new.blacklight_config }
   before(:each) do
     helper.extend Blacklight::Controller
--- spec/views/catalog/show.html.haml_spec.rb
+++ spec/views/catalog/show.html.haml_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
 describe 'catalog/show.html.haml' do
-  let(:document) { SolrDocument.new(id:"documentId","sourceResource_type_s"=>"image", "isShownAt_s"=>"http://dlg.galileo.usg.edu/dlg/aaed/do-th:aarl89.017-004-012", "subject_topic_facet"=>["subject1", "subject2"]) }
+  let(:document) { SolrDocument.new(id:"documentId","sourceResource_type_ssi"=>"image", "isShownAt_ssi"=>"http://dlg.galileo.usg.edu/dlg/aaed/do-th:aarl89.017-004-012", "subject_ssim"=>["subject1", "subject2"]) }
   let(:related_document_url) { "http://dlg.galileo.usg.edu/aaed/thumbs/aarl89.017-001-014.jpg" }
   let(:related_document_title) { "Sample related image title" }
-  let(:related_document) { SolrDocument.new(id:"relatedDocumentId","sourceResource_type_s"=>"image", "object_s"=>related_document_url, "title_display"=>related_document_title) }
+  let(:related_document) { SolrDocument.new(id:"relatedDocumentId","sourceResource_type_ssi"=>"image", "object_ssi"=>related_document_url, "title_ssi"=>related_document_title) }
 
   before do
     assign(:document,document)
@@ -17,8 +17,8 @@ describe 'catalog/show.html.haml' do
   it "renders subjects" do
     render file: 'catalog/show'#, locals: { document: document }
     expect(rendered).to have_selector(".subjects")
-    expect(rendered).to have_link("subject1",'/?f%5Bsubject_topic_facet%5D%5B%5D=subject1')
-    expect(rendered).to have_link("subject2",'/?f%5Bsubject_topic_facet%5D%5B%5D=subject2')
+    expect(rendered).to have_link("subject1",'/?f%5Bsubject_ssim%5D%5B%5D=subject1')
+    expect(rendered).to have_link("subject2",'/?f%5Bsubject_ssim%5D%5B%5D=subject2')
   end
   it "renders related content from more_like_this" do
     render file: 'catalog/show'#, locals: { document: document }

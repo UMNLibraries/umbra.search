d0f86ef2c9f76ea46209a9fc72112686652c3d22
chadfennell <libsys@gmail.com>
2015-05-19 11:59:48 -0500

    adds featured board routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   resources :featured_images
+  resources :featured_boards
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :controllers => { :registrations => "registrations" }

8b803506a89e91d858449b76d2cde4f68bc11440
chadfennell <libsys@gmail.com>
2015-06-25 11:27:57 -0500

    fixes regression: render_selected_facet_value removed during development of flags feature

--- app/helpers/facets_helper.rb
+++ app/helpers/facets_helper.rb
@@ -34,6 +34,19 @@ module FacetsHelper
   end
 
 
+  ##
+  # Standard display of a SELECTED facet value (e.g. without a link and with a remove button)
+  # @params (see #render_facet_value)
+  def render_selected_facet_value(facet_solr_field, item)
+    content_tag(:span, :class => "facet-label") do
+      val = facet_display_value(facet_solr_field, item)
+      content_tag(:span, val, :class => "selected #{type_class(val)}") +
+      # remove link
+      link_to(content_tag(:span, '', :class => "glyphicon glyphicon-remove") + content_tag(:span, '[remove]', :class => 'sr-only'),
+        search_action_path(remove_facet_params(facet_solr_field, item, params)), :class=>"remove")
+    end + render_facet_count(item.hits, :classes => ["selected"])
+  end
+
 
   ##
   # Standard display of a facet value in a list. Used in both _facets sidebar

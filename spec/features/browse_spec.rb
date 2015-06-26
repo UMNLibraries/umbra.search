467044b033ad73af9701667097821052eeead9b6
chadfennell <libsys@gmail.com>
2015-06-26 13:34:52 -0500

    account for the fact that the keyword facet now has a mincount

--- spec/features/browse_spec.rb
+++ spec/features/browse_spec.rb
@@ -3,13 +3,13 @@ require 'rails_helper'
 describe 'Browsing Facets' do
   let(:solr_docs) {
     docs = []
-    (0..200).each do
+    (0..300).each do
       random_text = rand(500).to_s
       docs << SolrDocument.new({
         id: random_text,
         title_ssi: random_text,
         creator_ssim: [random_text],
-        subject_ssim: [random_text],
+        subject_ssim: [:foo, :bar].sample,
         dataProvider_ssi: random_text,
         sourceResource_spatial_state_ssi: random_text,
         sourceResource_collection_title_ssi: random_text,
@@ -33,20 +33,26 @@ describe 'Browsing Facets' do
   it 'should browse each full facet view and receive 100 facet results sorted numerically' do
     facets = {
       "creator_ssim" => "Creator",
-      "subject_ssim" => "Keyword",
       "dataProvider_ssi" => "Contributing Institution",
       "sourceResource_spatial_state_ssi" => "Location",
       "sourceResource_collection_title_ssi" => "Collection"
     }
     facets.each do |id, name|
-      browse_facet(id, name)
+      browse_facet(id, name, 100)
     end
   end
 
-  def browse_facet(id, name)
+  context 'given the facet.mincount setting for subjects in catalog_controller.rb' do
+    it 'should browse the subject facet and recieve two results' do
+      visit "/catalog/facet/subject_ssim?limit=100"
+      browse_facet('subject_ssim', 'Keyword', 2)
+    end
+  end
+
+  def browse_facet(id, name, count)
     visit "/catalog/facet/#{id}?limit=100"
     expect(page).to have_content(name)
-    expect(page).to have_selector('.facet-label', count: 100)
+    expect(page).to have_selector('.facet-label', count: count)
     expect(find('.top .sort_options > .active')).to have_content("Numerical Sort")
   end
 end

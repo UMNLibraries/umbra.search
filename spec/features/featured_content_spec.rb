b8f55affaf0b01d43b1c01efb440b51bd50dbf24
chadfennell <libsys@gmail.com>
2015-08-12 13:46:44 -0500

    we pulled description from the UI

--- spec/features/featured_content_spec.rb
+++ spec/features/featured_content_spec.rb
@@ -5,12 +5,10 @@ feature 'Admin user creates a featured image' do
     sign_in(:user, :with_editor_role)
     visit new_featured_content_path
     fill_in 'Title', with: 'This is a title'
-    fill_in 'Description', with: 'Here is some description'
     check('Published')
     attach_file('featured_content_preview_image', "#{Rails.root}/spec/support/images/example.jpg")
     click_button 'Create Featured Content'
     expect(page).to have_content 'This is a title'
-    expect(find(:css, '.featured-content-description').text).to have_content 'Here is some description'
     expect(page.find('.featured-content-image')['src']).to have_content 'example.jpg'
     visit root_path
     expect(page).to have_content 'This is a title'

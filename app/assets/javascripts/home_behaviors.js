98b5c454d2bb9dbe1a3ecf799a3ab40c8d0994b2
chadfennell <libsys@gmail.com>
2015-08-18 12:18:10 -0500

    sets default focus on search forms

--- app/assets/javascripts/home_behaviors.js
+++ app/assets/javascripts/home_behaviors.js
@@ -1,10 +1,12 @@
-
 $(document).ready(function() {
+  $('.blacklight-pages-home input[type=text]').focus();
   $(window).scroll(function() {
       if ($(this).scrollTop() > 100) {
+        $('.blacklight-pages-home .navbar-primary input[type=text]').focus();
         $('.blacklight-pages-home .navbar-primary').fadeIn(10);
         $('.blacklight-pages-home .home-panel #home-search').fadeOut(120);
       } else {
+        $('.blacklight-pages-home input[type=text]').focus();
         $('.blacklight-pages-home .navbar-primary').fadeOut(10);
         $('.blacklight-pages-home .home-panel #home-search').fadeIn(100);
       }
---
+++ app/assets/javascripts/search.js
@@ -0,0 +1,3 @@
+$(document).ready(function() {
+  $('.search-query-form input[type=text]').focus();
+});

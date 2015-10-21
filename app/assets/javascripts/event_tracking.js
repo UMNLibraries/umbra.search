6eaaa26bb27143d86b0efdde1dd39e125fef6f14
Chad Fennell <fenne035@umn.edu>
2015-10-21 17:21:41 -0500

    refactors events

--- app/assets/javascripts/event_tracking.js
+++ app/assets/javascripts/event_tracking.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
+
+  function umbra_serialize_event_value(data) {
+    return data + '|' + window.location.href + '|' + window.location.pathname
+  }
+
+  function umbra_ga_event(category, action, label) {
+    ga('send', {
+      hitType: 'event',
+      eventCategory: category,
+      eventAction:  action,
+      eventLabel: umbra_serialize_event_value(label)
+    });
+  }
+
   if ($(".blacklight-catalog-show").length > 0) {
     var metadata = JSON.parse($(".metadata").attr('data-metadata')); 
     ga('send', 'pageview', {
@@ -13,252 +27,104 @@ $(document).ready(function(){
       'dimension3': metadata['import_job_name_ssi']
     });
 
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Record View',
-      eventAction: 'Full Record',
-      eventLabel: metadata['id']
-    });
+    umbra_ga_event('Record View', 'Full Record', metadata['id']);
   }
 
   $(".pin-it-large a").click(function() {
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Social Media',
-      eventAction: 'Pinterest Full Record',
-      eventLabel: window.location.href 
-    });
+    umbra_ga_event('Social Media', 'Pinterest Full Record', '');
   });
   
   $(".view-original-full-record").click(function() {
     var metadata = JSON.parse($(".metadata").attr('data-metadata'));
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'View Original',
-      eventAction: 'Full Record Click',
-      eventLabel: metadata['id']
-    });
+    umbra_ga_event('View Original', 'Full Record Click', metadata['id']);
   });
 
   $(".view-original-search").click(function() {
-    var id      = $(this).attr('data-document-id');
-    var context = $(this).attr('data-context');
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'View Original',
-      eventAction: context,
-      eventLabel: id
-    });
+    umbra_ga_event('View Original', $(this).attr('data-context'), $(this).attr('data-document-id'));
   });
 
   $("#facet-creator_ssim a.facet_select").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Facet',
-      eventAction: 'Creator Facet Select',
-      eventLabel: text
-    });
+    umbra_ga_event('Facet', 'Creator Facet Select', $(this).text());
   });
 
   $("#facet-sourceresource_type_ssi a.facet_select").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Facet',
-      eventAction: 'Type Facet Select',
-      eventLabel: text
-    });
+    umbra_ga_event('Facet', 'Type Facet Select', $(this).text());
   });
 
   $("#facet-dataprovider_ssi a.facet_select").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Facet',
-      eventAction: 'Contributing Institution Facet Select',
-      eventLabel: text
-    });
+    umbra_ga_event('Facet', 'Contributing Institution Facet Select', $(this).text());
   });
 
   $("#facet-subject_ssim a.facet_select").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Facet',
-      eventAction: 'Subject Institution Facet Select',
-      eventLabel: text
-    });
+    umbra_ga_event('Facet', 'Subject Facet Select', $(this).text());
   });
 
   $("#facet-sourceresource_spatial_state_ssi a.facet_select").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Facet',
-      eventAction: 'Location Facet Select',
-      eventLabel: text
-    });
+    umbra_ga_event('Facet', 'Location Facet Select', $(this).text());
   });
 
   $("#facet-sourceresource_date_begin_ssi a.facet_select").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Facet',
-      eventAction: 'Year Facet Select',
-      eventLabel: text
-    });
+    umbra_ga_event('Facet', 'Year Facet Select', $(this).text());
   });
 
   $(".featured-content a").click(function() {
-    var url = $(this).attr('href');
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Home Page',
-      eventAction: 'Featured Content Click',
-      eventLabel: url
-    });
+    umbra_ga_event('Featured Content Click', 'Home Page', $(this).attr('href'));
   });
 
   $(".search-thumbnail a").click(function() {
-    var url = $(this).attr('href');
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Search Thumbnail',
-      eventAction: 'List View Click',
-      eventLabel: url
-    });
+    umbra_ga_event('Search Thumbnail', 'List View Click', $(this).attr('href'));
   });
 
   $(".blacklight-catalog-index .cover-image a").click(function() {
-    var url = $(this).attr('href');
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Search Thumbnail',
-      eventAction: 'Gallery View Click',
-      eventLabel: url
-    });
+    umbra_ga_event('Search Thumbnail', 'Gallery View Click', $(this).attr('href'));
   });
 
   $(".blacklight-catalog-index .documents-list .title a").click(function() {
-    var url = $(this).attr('href');
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Search Record Title',
-      eventAction: 'List View Click',
-      eventLabel: url
-    });
+    umbra_ga_event('Search Record Title', 'List View Click', $(this).attr('href'));
   });
 
   $("#metadata .related a").click(function() {
-    var url = $(this).attr('href');
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Full Record Sidebar',
-      eventAction: 'Related Content Click',
-      eventLabel: url
-    });
+    umbra_ga_event('Full Record Sidebar', 'Related Content Click', $(this).attr('href'));
   });
 
   $("#metadata .subjects a").click(function() {
-    var url = $(this).attr('href');
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Full Record Sidebar',
-      eventAction: 'subjects Click',
-      eventLabel: url
-    });
+    umbra_ga_event('Full Record Sidebar', 'subjects Click', $(this).attr('href'));
   });
 
   $(".blacklight-pages-home #home-search .search-btn").click(function() {
-    var text = $("#home-search #q").val();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Search',
-      eventAction: 'Home Page Jumbo Search',
-      eventLabel: text
-    });
+    umbra_ga_event('Search', 'Home Page Jumbo Search', $("#home-search #q").val());
   });
 
   $(".blacklight-pages-home #primary-nav .search-btn").click(function() {
-    var text = $("#primary-nav #q").val();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Search',
-      eventAction: 'Home Page Navbar Search',
-      eventLabel: text
-    });
+    umbra_ga_event('Search', 'Home Page Navbar Search', $("#primary-nav #q").val());
   });
 
   $(".blacklight-catalog-index .search-btn").click(function() {
-    var text = $("#q").val();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Search',
-      eventAction: 'Catalog Navbar Search',
-      eventLabel: text
-    });
+    umbra_ga_event('Search', 'Catalog Navbar Search', $("#q").val());
   });
 
   $(".blacklight-pages-home .home-panel .browse-options a").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Browse Options Dropdown',
-      eventAction: 'Home Page Jumbo',
-      eventLabel: text
-    });
+    umbra_ga_event('Browse Options Dropdown', 'Home Page Jumbo', $(this).text());
   });
 
   $(".blacklight-pages-home #primary-nav  .browse-options a").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Browse Options Dropdown',
-      eventAction: 'Home Page Navbar',
-      eventLabel: text
-    });
+    umbra_ga_event('Browse Options Dropdown', 'Home Page Navbar', $(this).text());
   });
 
   $(".blacklight-catalog .browse-options a").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Browse Options Dropdown',
-      eventAction: 'Catalog Page Navbar',
-      eventLabel: text
-    });
+    umbra_ga_event('Browse Options Dropdown', 'Catalog Page Navbar', $(this).text());
   });
 
   $(".blacklight-pages-home .home-panel a.help-link").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Help Links',
-      eventAction: 'Home Page Jumbo',
-      eventLabel: text
-    });
+    umbra_ga_event('Help Links', 'Home Page Jumbo', $(this).text());
   });
 
   $(".blacklight-pages-home #help a").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Help Links',
-      eventAction: 'Home Page Navbar',
-      eventLabel: text
-    });
+    umbra_ga_event('Help Links', 'Home Page Navbar', $(this).text());
   });
 
   $(".blacklight-catalog #help a").click(function() {
-    var text = $(this).text();
-    ga('send', {
-      hitType: 'event',
-      eventCategory: 'Help Links',
-      eventAction: 'Catalog Page Navbar',
-      eventLabel: text
-    });
+    umbra_ga_event('Help Links', 'Catalog Page Navbar', $(this).text());
   });
 
 });

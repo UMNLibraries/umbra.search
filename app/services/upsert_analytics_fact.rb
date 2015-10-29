0d29bb9fb0dce87d4e0e6689caa3ec531b693086
Chad Fennell <fenne035@umn.edu>
2015-10-29 11:47:54 -0500

    prevent dups on subsequent ingests

--- app/services/upsert_analytics_fact.rb
+++ app/services/upsert_analytics_fact.rb
@@ -3,12 +3,12 @@ class UpsertAnalyticsFact
     record_hash = params.fetch(:record_hash)
     metadata = params.fetch(:metadata)
     if metadata
-      data_provider = DataProvider.where(name: metadata['dataProvider_ssi']).first_or_create
-      record   = Record.where(record_hash: record_hash, data_provider_id: data_provider.id).first_or_create
-      category = GoogleAnalyticsCategory.where(name: params.fetch(:category)).first_or_create
-      action   = GoogleAnalyticsAction.where(name: params.fetch(:action), google_analytics_category_id: category.id).first_or_create
-      location = Location.where(hostname: params.fetch(:hostname), path: params.fetch(:path)).first_or_create
-      GoogleAnalyticsFact.where(record_id: record.id, google_analytics_action_id: action.id, location_id: location.id, date: params.fetch(:date), count: params.fetch(:count)).first_or_create
+      data_provider = DataProvider.find_or_create_by(name: metadata['dataProvider_ssi'])
+      record   = Record.find_or_create_by(record_hash: record_hash, data_provider_id: data_provider.id)
+      category = GoogleAnalyticsCategory.find_or_create_by(name: params.fetch(:category))
+      action   = GoogleAnalyticsAction.find_or_create_by(name: params.fetch(:action), google_analytics_category_id: category.id)
+      location = Location.find_or_create_by(hostname: params.fetch(:hostname), path: params.fetch(:path))
+      GoogleAnalyticsFact.find_or_create_by(record_id: record.id, google_analytics_action_id: action.id, location_id: location.id, date: params.fetch(:date), count: params.fetch(:count))
     end
   end
 end

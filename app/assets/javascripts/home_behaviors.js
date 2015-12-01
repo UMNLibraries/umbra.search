69cf84753b212958df3b0f78fb02328f688b33de
Chad Fennell <fenne035@umn.edu>
2015-12-01 12:09:41 -0600

    fixes home page mobile UI

--- app/assets/javascripts/home_behaviors.js
+++ app/assets/javascripts/home_behaviors.js
@@ -1,14 +1,21 @@
 $(document).ready(function() {
   $('.blacklight-pages-home input[type=text]').focus();
-  $(window).scroll(function() {
-      if ($(this).scrollTop() > 100) {
-        $('.blacklight-pages-home .navbar-primary input[type=text]').focus();
-        $('.blacklight-pages-home .navbar-primary').fadeIn(10);
-        $('.blacklight-pages-home .home-panel #home-search').fadeOut(120);
-      } else {
-        $('.blacklight-pages-home input[type=text]').focus();
-        $('.blacklight-pages-home .navbar-primary').fadeOut(10);
-        $('.blacklight-pages-home .home-panel #home-search').fadeIn(100);
-      }
-  });
+  var isMobile = window.matchMedia("only screen and (max-width: 768px)");
+  if (isMobile.matches == false) {
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+          $('.blacklight-pages-home .navbar-primary input[type=text]').focus();
+          $('.blacklight-pages-home .navbar-primary').fadeIn(10);
+          $('.blacklight-pages-home .home-panel #home-search').fadeOut(120);
+        } else {
+          $('.blacklight-pages-home input[type=text]').focus();
+          $('.blacklight-pages-home .navbar-primary').fadeOut(10);
+          $('.blacklight-pages-home .home-panel #home-search').fadeIn(100);
+        }
+    });
+  } else {
+      $('.blacklight-pages-home .home-panel #home-search').display();
+      $('.blacklight-pages-home .home-panel #home-search').focus();
+      $('.blacklight-pages-home .home-panel #home-search').focus();
+   }
 });

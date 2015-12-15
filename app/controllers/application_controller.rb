b23cb9acc49d30332e653ad358a3400bc25c750d
Chad Fennell <fenne035@umn.edu>
2015-12-15 15:16:05 -0600

    removes unused code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,11 +8,6 @@ class ApplicationController < ActionController::Base
 
   layout 'umbra' # use layouts/umbra instead of default layouts/blacklight
 
-  # Adds a few additional behaviors into the application controller 
-   include Blacklight::Controller
-  # Please be sure to impelement current_user and user_session. Blacklight depends on 
-  # these methods in order to perform user specific actions. 
-
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception

a38f065ac01a9863cbc2de8fbbec203ab7e31135
Chad Fennell <fenne035@umn.edu>
2016-04-12 14:42:32 -0500

    fixes jsonapi spec

--- app/services/index_from_json_api.rb
+++ app/services/index_from_json_api.rb
@@ -3,23 +3,19 @@ require 'digest/sha1'
 class IndexFromJsonApi
   def self.run!(query_url)
     results = JSON.parse(fetch_url(query_url))
-    docs = []
-    results['data'].each do |result|
-      docs <<  json_api_to_solr(result)
-    end
-    SolrClient.add(docs)
-    Rails.logger.info "Ingesting another batch of #{docs.length} solr docs from query: #{query_url}"
+    SolrClient.add(results['data'].map { |result| json_api_to_solr(result) })
+    Rails.logger.info "Ingesting another batch of #{results['data'].length} solr docs from query: #{query_url}"
     yield results['links'].fetch('next', false) if block_given?
   end
 
   def self.json_api_to_solr(result)
     attrs = result['attributes']
     doc   = attrs['metadata']['HUBINDEX']
-    doc.merge({'id' => attrs['record-hash'], 
-               'import_job_name_ssi' => attrs['import-job-name'], 
+    doc.merge({'id' => attrs['record-hash'],
+               'import_job_name_ssi' => attrs['import-job-name'],
                'tags_ssim' => attrs['import-job-tags']})
   end
-    
+
   def self.fetch_url(url)
       raise "Must Provide a URL to fetch remote data" if !url
       start_time = Time.now

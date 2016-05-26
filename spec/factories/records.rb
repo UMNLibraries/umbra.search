ea43477a71e423ee5cc82430633fd4b4283d9eb0
Chad Fennell <fenne035@umn.edu>
2016-05-26 15:31:31 -0500

    new trait to support testing subject metadata

--- spec/factories/records.rb
+++ spec/factories/records.rb
@@ -12,6 +12,12 @@ FactoryGirl.define do
         (0..1).map { |delta| record.record_tags << FactoryGirl.build(:record_tag, tag: FactoryGirl.build(:tag, name: "blerb #{delta}"), record: record) }
       end
     end
+
+    trait :with_metadata_with_subjects do
+      metadata '{"subject_ssim": ["Civil War", "Civil Rights", "Civil Service"]}'
+    end
+
+    factory :record_with_keywords, :traits => [:with_editor_tags, :with_metadata_with_subjects]
   end
 
 end

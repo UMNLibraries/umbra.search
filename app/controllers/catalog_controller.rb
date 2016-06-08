5565abf7ff02f5e84610812da0534ac8743f0fcc
Chad Fennell <fenne035@umn.edu>
2016-06-08 09:35:10 -0500

    ensure search results have keyword data

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -74,6 +74,7 @@ class CatalogController < ApplicationController
     # solr field configuration for search results/index views
     config.index.title_field = 'title_ssi'
     config.index.display_type_field = 'sourceResource_type_ssi'
+    config.index.display_type_field = 'keywords_ssim'
     config.index.thumbnail_method = :cached_thumbnail_tag
     config.show.thumbnail_method = :cached_thumbnail_tag
 

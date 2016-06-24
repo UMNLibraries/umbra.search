f7150df3101826876695d14fc9bdadb9b64c54f9
Chad Fennell <fenne035@umn.edu>
2016-06-24 13:34:11 -0500

    use local thumbnails from the asset pipeline and remove a slow blocking request from the process. If the example_search model becomes an active record model, I'll add an uploader and load images from the uploads dir

--- app/helpers/example_searches_helper.rb
+++ app/helpers/example_searches_helper.rb
@@ -18,7 +18,7 @@ module ExampleSearchesHelper
         title: "Civil Rights in the 1800's",
         q: "1800's civil rights",
         fq: 'keywords_ssim:"Civil Rights"',
-        thumbnail: 'https://www.umbrasearch.org/thumbnail?url=http%3A%2F%2Fcredo.library.umass.edu%2Fimages%2Fresize%2F175x200%2Fmums312-b287-i016-001.png'
+        thumbnail: 'dubois.png'
       }
     ]
   end

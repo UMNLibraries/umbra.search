d28170b6950b6362f3c1a56ad9a95b2b26195866
Chad Fennell <fenne035@umn.edu>
2016-06-24 12:18:56 -0500

    it would be nice if the example searches didn't disappear the moment you stop hovering on the browse dropdown. makes that happen

--- app/assets/javascripts/home_behaviors.js
+++ app/assets/javascripts/home_behaviors.js
@@ -19,7 +19,13 @@ $(document).ready(function() {
       $('.blacklight-pages-home .home-panel #home-search').focus();
    }
 
-  $('.example-searches button').hover( function() {
-    $(this).dropdown('toggle');
-  });
+  $('.example-searches').hover(
+    function() {
+      $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
+      $(this).toggleClass('open');
+    },
+    function() {
+      $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
+      $(this).toggleClass('open');
+    });
 });

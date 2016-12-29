d74d4abd851b8c0548fce8de9ae6b31b8b68b4ff
chadfennell <libsys@gmail.com>
2016-12-29 09:36:09 -0600

    update cap version, renamed github repo

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid only for current version of Capistrano
-lock '3.2.1'
+lock '3.7.1'
 
 set :scm, :git
-set :repo_url, 'git@github.com:UMNLibraries/aath.search.git'
+set :repo_url, 'git@github.com:UMNLibraries/umbra.search.git'
 # to specify a specific branch when deploying:
 # `BRANCH_NAME=[branch] cap production deploy`
 set :branch, ENV["BRANCH_NAME"] || "master"

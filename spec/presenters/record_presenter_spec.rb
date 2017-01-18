3db27e3cb9725f477c94fb88bc3800d08207a92a
chadfennell <libsys@gmail.com>
2017-01-18 13:43:25 -0600

    update the record presenter test to the new flag vote api

--- spec/presenters/record_presenter_spec.rb
+++ spec/presenters/record_presenter_spec.rb
@@ -7,14 +7,17 @@ describe RecordPresenter do
   let(:flag_vote) { double }
   let(:metadata) { JSON.parse(record.metadata).merge('id' => record.id) }
 
+  before(:all) do
+    Rails.cache.clear
+  end
+
   it "encodes a record for ingest into solr" do
    WebMock.disable_net_connect!
    stub_request(:get, "http://127.0.0.1:8889/solr/blacklight-core-umbra-0/select?fl=*&q=id:123abc&rows=10&start=0&wt=ruby").
          to_return(:status => 200, :body => %Q({"response" => {"docs" => [#{metadata}]}}), :headers => {})
-   stub_request(:get, "https://www.umbrasearch.org/flag_votes.json?by_record_id=true").
+   stub_request(:get, "https://www.umbrasearch.org/flag_votes.json?flags_by_record=true").
      with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'Host'=>'www.umbrasearch.org', 'User-Agent'=>'Ruby'}).
      to_return(:status => 200, :body => "{\"#{record.id}\": [#{flag.id}]}", :headers => {})
-
 
     allow(flag_vote).to receive(:flag) { flag }
     allow(flag_vote).to receive(:where) { [flag_vote] }

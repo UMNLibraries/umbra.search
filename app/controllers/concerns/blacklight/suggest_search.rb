a871bdcff79e1c4a8a94665e4d5cd654bc6fa228
chadfennell <libsys@gmail.com>
2017-01-27 15:29:59 -0600

    new suggest system uses a default dictionary, so no need to specify

--- app/controllers/concerns/blacklight/suggest_search.rb
+++ app/controllers/concerns/blacklight/suggest_search.rb
@@ -21,7 +21,7 @@ module Blacklight
     # Query the suggest handler using RSolr::Client::send_and_receive
     # @return [RSolr::HashWithResponse]
     def suggest_results
-      Blacklight.default_index.connection.send_and_receive(suggest_handler_path, params: request_params.merge({'suggest.dictionary' => dictionary_param}))
+      Blacklight.default_index.connection.send_and_receive(suggest_handler_path, params: request_params)
     end
 
     ##
@@ -30,10 +30,5 @@ module Blacklight
       CatalogController.blacklight_config.autocomplete_path
     end
 
-    private
-
-    def dictionary(params)
-      params.fetch('suggest.dictionary', 'search_suggest')
-    end
   end
 end

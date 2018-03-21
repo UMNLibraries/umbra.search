b0deb8fb867652364ff9fce8ea7ccdd04f423d7e
chadfennell <libsys@gmail.com>
2018-03-21 10:55:57 -0500

    update spec to latest thumbnail helper api

--- spec/helpers/thumbnail_helper_spec.rb
+++ spec/helpers/thumbnail_helper_spec.rb
@@ -12,7 +12,7 @@ describe ThumbnailHelper, :type => :helper do
     it "should generate a url for requesting the cached thumb from the ThumbnailsController" do
       rendered_text = helper.cached_thumbnail_tag(document, {})
       rendered_html = Capybara::Node::Simple.new(rendered_text)
-      expect( rendered_html ).to have_xpath("//img[@src=\"#{cached_thumbnail_url(url:thumbnail_url)}\" and @title='Awesome Title' and @alt='Awesome Title']")
+      expect( rendered_html ).to have_xpath("//img[@src=\"#{cached_thumb_url(thumbnail_url)}\" and @title='Awesome Title' and @alt='Awesome Title']")
     end
   end
 end

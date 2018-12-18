749c2e0283cc006678beeb1fe8d7551a30997d1f
chadfennell <libsys@gmail.com>
2018-12-18 14:38:47 -0800

    upcase data from a few specific technical metadata fields as requested

--- app/helpers/show_record_helper.rb
+++ app/helpers/show_record_helper.rb
@@ -6,4 +6,22 @@ module ShowRecordHelper
   def set_page_title!
     @page_title = t('blacklight.search.show.title', :document_title => document_show_html_title, :application_name => application_name).html_safe
   end
+
+  def show_field_label(doc, name)
+    render_document_show_field_label doc, field: name
+  end
+
+  def show_field_value(doc, name)
+    value = render_document_show_field_value doc, field: name
+    label = show_field_label(doc, name)
+    if titlizables.include? label
+      value.titleize
+    else
+      value
+    end
+  end
+
+  def titlizables
+    ['Format:', 'Created Date:', 'Type:']
+  end
 end

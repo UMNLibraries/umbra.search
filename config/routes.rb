08907aa8feb33664a88dc0690fb29f8a733aa65e
Chad Fennell <libsys@gmail.com>
2019-05-30 16:28:33 -0500

    new BL routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,29 @@
 Rails.application.routes.draw do
 
+
+  root :to => 'pages#home'
+  mount Blacklight::Engine => '/'
+  # root to: "catalog#index"
+  concern :searchable, Blacklight::Routes::Searchable.new
+
+  resource :catalog, only: [:index], as: 'catalog', path: '/catalog', controller: 'catalog' do
+    concerns :searchable
+  end
+
+  concern :exportable, Blacklight::Routes::Exportable.new
+
+  resources :solr_documents, only: [:show], path: '/catalog', controller: 'catalog' do
+    concerns :exportable
+  end
+
+  resources :bookmarks do
+    concerns :exportable
+
+    collection do
+      delete 'clear'
+    end
+  end
+
   resources :pages
   resources :featured_images
   resources :flags
@@ -25,9 +49,6 @@ Rails.application.routes.draw do
   get 'featured_board/preview' => 'featured_boards#preview', as: 'board_preview'
 
   devise_for :users, :controllers => { :registrations => "registrations" }
-  root :to => 'pages#home'
-
-  blacklight_for :catalog
 
 
   get '/about', to: redirect('/pages/about')
@@ -41,68 +62,10 @@ Rails.application.routes.draw do
   get '/about/inspirations', to: redirect('/pages/inspirations')
 
   get 'contribute' => 'catalog#contribute', as: 'contribute'
-  # get 'contact' => 'catalog#contact', as: 'contact'
-  # post  'users/:id/update' => 'users#update', as: 'update_user'
-  # patch 'users/:id/update' => 'users#update'
   resources :users
   resources :featured_contents
 
   resources :suggest, only: :index, defaults: { format: 'json' }
 
   get 'thumbnail_cache/:record_id' => 'thumbnail_cache#update', as: 'thumbnail_cache'
-
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end

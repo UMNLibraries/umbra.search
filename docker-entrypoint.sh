3bffcb220b12a3a7435a8cc2049bc4b1cd9060fb
Chad Fennell <libsys@gmail.com>
2019-05-30 16:33:43 -0500

    dockerfile expects and entrypoint

---
+++ docker-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+# Exit on fail
+
+if [ -f tmp/pids/server.pid ]; then
+  rm tmp/pids/server.pid
+fi
+
+# Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
+
+exec "$@"
+# Finally call command issued to the docker service

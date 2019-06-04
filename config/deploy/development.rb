31291d218c33081f643bc000e4afa0dd17ac8c70
Chad Fennell <libsys@gmail.com>
2019-06-04 15:04:07 -0500

    deploy the blacklight 6.20 branch; reboot sidekiq with systemd now

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,7 +1,6 @@
 server "lib-umbra-dev-01.oit.umn.edu", user: "swadm", roles: %w{app db web}
 set :deploy_to, "/swadm/var/www/app"
 set :use_sudo, false
-set :rails_env, "production"
 set :bundle_flags, '--deployment'
-set :keep_releases, 2
-set :branch, 'updates'
+set :keep_releases, 4
+set :branch, 'bl6.20'

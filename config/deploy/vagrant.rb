101cc7bafc542d3fd4b0a85fd0bb786c6afd21e4
Chad Fennell <libsys@gmail.com>
2019-06-06 11:35:25 -0500

    systemd handles sidekiq reboots now

--- config/deploy/vagrant.rb
+++ config/deploy/vagrant.rb
@@ -1,7 +1,7 @@
-server "192.168.33.12", user: "swadm", roles: %w{app db web}
+server "ansible_umbra", user: "swadm", roles: %w{app db web}
 set :deploy_to, "/swadm/var/www/app"
 set :use_sudo, false
 set :rails_env, "production"
 set :bundle_flags, '--deployment'
 set :keep_releases, 2
-set :branch, 'updates'
+set :branch, 'bl6.20'

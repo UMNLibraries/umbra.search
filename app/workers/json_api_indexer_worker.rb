269205319ae4d1ed379a7ebb6ff9d38de5ffa547
Chad Fennell <libsys@gmail.com>
2019-06-20 13:09:12 -0500

    removes recursion - the worker now just expects to get a single url

--- app/workers/json_api_indexer_worker.rb
+++ app/workers/json_api_indexer_worker.rb
@@ -1,8 +1,6 @@
 class JsonApiIndexerWorker
   include Sidekiq::Worker
   def perform(url)
-    IndexFromJsonApi.run!(url) {|next_url|
-      JsonApiIndexerWorker.perform_async(next_url) if next_url
-    }
+    IndexFromJsonApi.run!(url)
   end
 end

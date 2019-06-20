e20c39e68df4136884e8638b586ea848322be0c8
Chad Fennell <libsys@gmail.com>
2019-06-20 11:16:56 -0500

    more features moved into solr config

--- spec/controllers/catalog_controller_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'rails_helper'
-
-describe CatalogController, :type => :controller do
-  it "should include 'more like this' behaviors" do
-    expect(controller.class.included_modules).to include BlacklightMoreLikeThis::SolrHelperExtension
-    expect(controller.class.search_params_logic).to include :add_solr_mlt_parameters
-  end
-end

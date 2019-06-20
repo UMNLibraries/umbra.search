1644f822389cb48c37173c4b29b654a4c65a4354
Chad Fennell <libsys@gmail.com>
2019-06-20 11:32:11 -0500

    remove debugging code

--- spec/controllers/flag_votes_controller_spec.rb
+++ spec/controllers/flag_votes_controller_spec.rb
@@ -10,19 +10,6 @@ describe FlagVotesController, :type => :controller do
 
   it "lists flag votes" do
     get :index
-    puts response.inspect
     expect(response).to have_http_status(:ok)
   end
-
-  # it "shows json display of flag vote records" do
-  #   get :show, {:id => flag_vote.id, :format => :json}
-  #   expect(response).to have_http_status(:ok)
-  # end
-
-  # it "it does not error out when a record is missing from the solr index" do
-  #   flag_vote.record_id = 'This_record_does_not_exist'
-  #   flag_vote.save
-  #   get :show, {:id => flag_vote.id, :format => :json}
-  #   expect(response).to have_http_status(:ok)
-  # end
 end

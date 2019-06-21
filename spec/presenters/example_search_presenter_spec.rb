55aac744ff1ab963633512219c912375974da9a7
Chad Fennell <libsys@gmail.com>
2019-06-21 13:24:13 -0500

    fixes broken example search presenter test

--- spec/presenters/example_search_presenter_spec.rb
+++ spec/presenters/example_search_presenter_spec.rb
@@ -3,8 +3,7 @@ require './app/presenters/example_search_presenter.rb'
 
 describe ExampleSearchPresenter do
   include ActionView::TestCase::Behavior
-
-  let(:example_search) { ExampleSearch.new(q: '*:*', fq: 'creator_ssi:Fred', title: 'A search for meaning.')}
+  let(:example_search) { ExampleSearch.new(q: '*:*', fq: ['creator_ssi:Fred'], title: 'A search for meaning.')}
   it "runs a search and returns the number found" do
     presenter = ExampleSearchPresenter.new(example_search, solr: solr)
     expect(presenter.items_found).to eq "101 items"
@@ -12,7 +11,7 @@ describe ExampleSearchPresenter do
 
   it "produces a url to a catalog search" do
     presenter = ExampleSearchPresenter.new(example_search, params: {rows: 101}, template: view)
-    expect(presenter.url).to eq "http://test.host/catalog?fq=creator_ssi%3AFred&q=%2A%3A%2A&rows=101"
+    expect(presenter.url).to eq "/catalog?rows=101&q=*:*&rows=101&f[creator_ssi][]=Fred&example-search="
   end
 
   def solr

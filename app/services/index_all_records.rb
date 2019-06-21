a9073bdc4c379c6db5942ae7454de1720d49c4ed
Chad Fennell <libsys@gmail.com>
2019-06-21 14:36:37 -0500

    make IndexAllRecords more easily testible...and test it

--- app/services/index_all_records.rb
+++ app/services/index_all_records.rb
@@ -1,7 +1,11 @@
 class IndexAllRecords
+  attr_reader :indexer_klass
+  def initialize(indexer_klass: IndexRecord)
+    @indexer_klass = indexer_klass
+  end
   def index!
     Record.find_in_batches(batch_size: 500) do |records|
-      records.map {|record| IndexRecord.new(record: record).index! }
+      records.map {|record| indexer_klass.new(record: record).index! }
     end
   end
 end
--- spec/services/index_all_records_spec.rb
+++ spec/services/index_all_records_spec.rb
@@ -3,24 +3,13 @@ require './app/services/index_all_records.rb'
 
 describe IndexAllRecords do
   let(:record) { create :record}
-  subject { IndexAllRecords.new }
+  let(:indexer_klass) { double }
+  let(:indexer_klass_obj) { double }
+  subject { IndexAllRecords.new(indexer_klass: indexer_klass) }
 
   it "Indexes all available records" do
-    WebMock.disable_net_connect!
-     record = create :record
-
-    stub_request(:get, "http://127.0.0.1:8889/solr/blacklight-core-umbra/select?fl=*&q=id:123abc&rows=10&start=0&wt=ruby").
-      to_return(:status => 200, :body => '{"response" => {"docs" => [{"foo" => "bar"}]}}', :headers => {})
-    stub_request(:post, "http://127.0.0.1:8889/solr/blacklight-core-umbra/update?wt=ruby").
-      with(:body => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><add><doc><field name=\"foo\">bar</field><field name=\"id\">123abc</field><field name=\"import_job_name_ssi\">Fake Provider</field></doc></add>",
-          :headers => {'Content-Type'=>'text/xml'})
-
+    expect(indexer_klass).to receive(:new).with(record: record) { indexer_klass_obj }
+    expect(indexer_klass_obj).to receive(:index!)
     subject.index!
-
-    expect(WebMock).to have_requested(:post, "http://127.0.0.1:8889/solr/blacklight-core-umbra/update?wt=ruby").
-      with(:body => "<?xml version=\"1.0\" encoding=\"UTF-8\"?><add><doc><field name=\"foo\">bar</field><field name=\"id\">123abc</field><field name=\"import_job_name_ssi\">Fake Provider</field></doc></add>",
-       :headers => {'Content-Type'=>'text/xml'})
   end
 end
-
-WebMock.allow_net_connect!

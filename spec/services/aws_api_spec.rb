28141db44a56b5b76342aa2069bb2e267b6bff30
Chad Fennell <libsys@gmail.com>
2019-06-21 14:12:23 -0500

    fixes broken AWS service test

--- spec/services/aws_api_spec.rb
+++ spec/services/aws_api_spec.rb
@@ -5,10 +5,12 @@ describe AwsApi do
   it "posts a URL " do
     url = 'https://example.com/foo?=blah'
     api_key = '12345678910112'
-    post_klass = double('Net::HTTP')
-    post_klass_obj = double('PostKlassObj')
-    expect(post_klass).to receive(:new).with(URI(url)) { {} }
-    post = AwsApi.new(url: url, api_key: api_key, post_klass: post_klass).post!
-    expect(post).to eq({"x-api-key" => "12345678910112"})
+    post_klass = double('Net::HTTP::Post')
+    http_klass = double('Net::HTTP')
+    http_klass_obj = double
+    expect(post_klass).to receive(:new).with(URI(url)) { {'x-api-key' => 'foo'} }
+    expect(http_klass).to receive(:start).with("example.com", 443, {:use_ssl=>true}).and_yield(http_klass_obj)
+    expect(http_klass_obj).to receive(:request).with({"x-api-key"=>"12345678910112"})
+    AwsApi.new(url: url, api_key: api_key, http_klass: http_klass, post_klass: post_klass).upload!
   end
 end

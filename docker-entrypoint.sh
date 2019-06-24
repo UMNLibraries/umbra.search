c7a756519c005766fd8c7da9bf7bb15228734e8a
Chad Fennell <libsys@gmail.com>
2019-06-24 13:36:55 -0500

    remove the test tmp pid as well since we now run a test endpoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,10 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
+if [ -f tmp/pids/server-test.pid ]; then
+  rm tmp/pids/server-test.pid
+fi
+
 # Ensure all gems installed. Add binstubs to bin which has been added to PATH in Dockerfile.
 
 exec "$@"

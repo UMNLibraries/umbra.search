013e10e4a0920231e4a4d6edbf494113dfef15a2
Chad Fennell <libsys@gmail.com>
2019-06-24 11:51:06 -0500

    removes unused code

--- spec/services/upsert_document_tags_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'rails_helper'
-
-describe UpsertDocumentTags do
-  let(:tags) { ['foo', 'bar', 'baz'] }
-  it "adds new tags and associates them with a document" do
-    UpsertDocumentTags.new(tags: tags, document_id: 1).replace_tags!
-    expect(DocumentTag.all.length).to eq 3
-  end
-end

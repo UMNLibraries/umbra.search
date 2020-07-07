a5e953c390fae1b336d5de16d0c9140400e6c551
Michael Berkowski <mjb@umn.edu>
2020-07-07 15:21:28 -0500

    separate umn deploy bits

--- config/deploy/development.rb
+++
@@ -1,5 +0,0 @@
-server "lib-umbra-dev-01.oit.umn.edu", user: "swadm", roles: %w{app db web}
-set :deploy_to, "/swadm/var/www/app"
-set :use_sudo, false
-set :bundle_flags, '--deployment'
-set :keep_releases, 4
--- config/deploy/vagrant.rb
+++
@@ -1,6 +0,0 @@
-server "ansible_umbra", user: "swadm", roles: %w{app db web}
-set :deploy_to, "/swadm/var/www/app"
-set :use_sudo, false
-set :rails_env, "production"
-set :bundle_flags, '--deployment'
-set :keep_releases, 2
